bf1c894e213cd644e67fe4c774ad377685879e0e
stevenks17 <stevenks17@gmail.com>
2020-10-22 16:28:34 -0400

    Jumbotron added to app render

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import jumboData from './fixtures/jumbo'
+import Jumbotron from './components/jumbotron'
 
 export default function App() {
   return (
-<h1>Hello</h1>
-  );
+    <Jumbotron.Container>
+      {jumboData.map((item) => (
+        <Jumbotron key={item.id} direction={item.direction}>
+          <p>Hello</p>
+        </Jumbotron>
+      ))}
+    </Jumbotron.Container>
+
+    );
 }
 
