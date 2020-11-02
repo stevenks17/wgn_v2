bd604532a0b99cc563786dd1549f1d2fb2e39d00
stevenks17 <stevenks17@gmail.com>
2020-11-02 18:34:22 -0500

    cleaned up spacing

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,33 +1,30 @@
-import React from 'react'
-import { Container, Row, Column, Link, Title, Text, Break } from './styles/footer'
+import React from 'react';
+import { Container, Row, Column, Link, Title, Text, Break } from './styles/footer';
 
-export default function Footer({children, ...restProps}) {
-    return <Container {...restProps}>{children}</Container>
+export default function Footer({ children, ...restProps }) {
+  return <Container {...restProps}>{children}</Container>;
 }
 
+Footer.Row = function FooterRow({ children, ...restProps }) {
+  return <Row {...restProps}>{children}</Row>;
+};
 
+Footer.Column = function FooterColumn({ children, ...restProps }) {
+  return <Column {...restProps}>{children}</Column>;
+};
 
-Footer.Row = function FooterRow({children, ...restProps}) {
-    return <Row {...restProps}>{children}</Row>
-}
+Footer.Link = function FooterLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
 
+Footer.Title = function FooterTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
 
-Footer.Column = function FooterColumn({children, ...restProps}) {
-    return <Column {...restProps}>{children}</Column>
-}
+Footer.Text = function FooterText({ children, ...restProps }) {
+  return <Text {...restProps}>{children}</Text>;
+};
 
-Footer.Link = function FooterLink({children, ...restProps}) {
-    return <Link {...restProps}>{children}</Link>
-}
-
-Footer.Title = function FooterTitle({children, ...restProps}) {
-    return <Title {...restProps}>{children}</Title>
-}
-
-Footer.Text = function FooterText({children, ...restProps}) {
-    return <Text {...restProps}>{children}</Text>
-}
-
-Footer.Break = function FooterBreak({children, ...restProps}) {
-    return <Break {...restProps}>{children}</Break>
-}
+Footer.Break = function FooterBreak({ ...restProps }) {
+  return <Break {...restProps} />;
+};
