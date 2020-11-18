989d3fb32f37ffb46b3a6f63f574b39483f8b642
stevenks17 <stevenks17@gmail.com>
2020-11-18 10:04:26 -0500

    routing test using React Router

---
+++ src/pages/browse.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+export default function Browse() {
+    
+  return  (<h1> Hello from the browse</h1>)
+
+}
---
+++ src/pages/signin.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+export default function Signin() {
+    
+  return  (<h1> Hello from the Signin</h1>)
+
+}
