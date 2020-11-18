67a48a9c09bf4fe130cf71416bfb56a81f92ec4e
stevenks17 <stevenks17@gmail.com>
2020-11-17 20:15:05 -0500

    corrected typo

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,11 +5,12 @@ import { JumbotronContainer } from '../containers/jumbotron'
 
 
 export default function Home() {
-
+return (
     <> 
         <JumbotronContainer />
         <FaqsContainer />
         <FooterContainer />
     
     </>
+ )
 }
