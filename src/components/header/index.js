941c748238cfa9242a2ce379b585750b941b67c6
stevenks17 <stevenks17@gmail.com>
2020-11-18 11:31:46 -0500

    created logo and imports

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import {Link as ReactRouterLink} from 'react-router-dom'
+import { Background, Container, Logo } from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? <Background {...restProps}>{children}</Background> : children;
@@ -10,3 +11,10 @@ Header.Frame = function HeaderFrame({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
 }
 
+Header.Logo = function HeaderLogo ({ to, ...restProps}) {
+    return (
+        <ReactRouterLink to={to}>
+            <Logo {...restProps} />
+        </ReactRouterLink>
+    )
+}
