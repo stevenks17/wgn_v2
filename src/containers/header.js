4b0036f67bd3a48a77de324e851443229c5fe95c
stevenks17 <stevenks17@gmail.com>
2020-11-18 11:13:25 -0500

    header container created

---
+++ src/containers/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Header } from '../components/accordion/styles/accordion'
+
+
+export default function HeaderContainer() {
+    return (
+        <Header>
+            <Header.Frame >
+                <Header.Logo />
+                <Header.Button />
+            </Header.Frame>
+        </Header>
+    )
+}
