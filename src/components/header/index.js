15c5d79bc8efb92df4072bb35756cede1be59d68
stevenks17 <stevenks17@gmail.com>
2020-11-18 11:28:04 -0500

    Created Frame with pass in for props and children

---
+++ src/components/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function Header({ bg = true, children, ...restProps }) {
+    return bg ? <Background {...restProps}>{children}</Background> : children;
+}
+
+
+Header.Frame = function HeaderFrame({children, ...restProps}) {
+    return <Container {...restProps}>{children}</Container>
+}
+
