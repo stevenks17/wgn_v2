c0dd8cfb09bb73767c9ca5e38cd3c62cca4d7681
stevenks17 <stevenks17@gmail.com>
2020-11-19 18:34:46 -0500

    styled container

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,15 +1,33 @@
 import styled from 'styled-components/macro'
 import { Link as ReactRouterLink } from 'react-router-dom'
 
-export const Background = styled.div``
+export const Background = styled.div`
+
+`
 
 export const Frame = styled.div``
 
-export const Container = styled.div``
+export const Container = styled.div`
+    display: flex;
+    margin: 0 56px;
+    height: 64px;
+    padding: 18px 0;
+    justify-content: space-between;
+    align-items: center;
+
+    a {
+        display:flex;
+    }
+
+    @media(max-width: 1000px) {
+        margin: 0 30px;
+    }
+`
 
 export const Logo = styled.img`
     width: 108px;
-    margin-right: 40px;
+    height: 52px;
+    margin-left: -40px;
 
     @media (min-width: 1449px) {
         height: 45px;
