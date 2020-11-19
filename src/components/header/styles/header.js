d0979f23a2d209c6e771a17d5c57f69ab1c8a686
stevenks17 <stevenks17@gmail.com>
2020-11-19 17:18:06 -0500

    styled logo size

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -7,7 +7,15 @@ export const Frame = styled.div``
 
 export const Container = styled.div``
 
-export const Logo = styled.img``
+export const Logo = styled.img`
+    width: 108px;
+    margin-right: 40px;
+
+    @media (min-width: 1449px) {
+        height: 45px;
+        width: 167px;
+    }
+`
 
 export const ButtonLink = styled(ReactRouterLink)`
     display: block;
@@ -21,8 +29,8 @@ export const ButtonLink = styled(ReactRouterLink)`
     cursor: pointer;
     text-decoration: none;
     box-sizing: border-box;
-
-
+    margin-left: 13px;
+    margin-top: 6px;
     &:hover{
         background-color: #f40612;
     }
