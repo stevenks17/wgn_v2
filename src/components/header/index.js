ebe0a233e78aa10fe7712739d1251b6a305b7ac6
stevenks17 <stevenks17@gmail.com>
2020-11-20 19:07:35 -0500

    finished background header return

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,8 +3,14 @@ import {Link as ReactRouterLink} from 'react-router-dom'
 import { Background, ButtonLink, Container, Logo } from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
-    return bg ? <Background {...restProps}>{children}</Background> : children;
-}
+    return bg ? (
+        <Background data-testid="header-bg" {...restProps}>
+          {children}
+        </Background>
+      ) : (
+        children
+      );
+    }
 
 
 Header.Frame = function HeaderFrame({children, ...restProps}) {
