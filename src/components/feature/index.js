7d9e8c7bfd3aa470a68ede7a851bc48b70a38399
stevenks17 <stevenks17@gmail.com>
2020-11-24 12:35:10 -0500

    created feature component

---
+++ src/components/feature/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, Title, SubTitle } from './styles/feature'
+
+export default function Feature ({children, ...restProps}) {
+    return <Container {...restProps}>{children} </Container>
+}
+
+Feature.Title = function FeatureTitle({children, ...restProps}) {
+    return <Title {}
+}
