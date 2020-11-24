763b08f72fc15dbdd88641c0f575c4ac63bd1f3a
stevenks17 <stevenks17@gmail.com>
2020-11-24 12:35:42 -0500

    re-used optform and added it to Header Container

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Feature, OptForm } from '../components'
 import { FaqsContainer } from '../containers/faqs'
 import { FooterContainer } from '../containers/footer'
 import HeaderContainer from '../containers/header'
@@ -9,10 +10,20 @@ export default function Home() {
 return (
     <> 
         <HeaderContainer>
+            <Feature>
+                <Feature.Title>All the newest games in one location!</Feature.Title>
+                <Feature.SubTitle> Get the latest info from fellow gamers</Feature.SubTitle>
+            </Feature>
+        <OptForm>
+              <OptForm.Input placeholder="Email Address" />
+              <OptForm.Button> Try it now </OptForm.Button>
+              <OptForm.Break />
+              <OptForm.Text> Get notifications for the most popular titles </OptForm.Text>
+          </OptForm>
+        </HeaderContainer >
             <JumbotronContainer />
             <FaqsContainer />
             <FooterContainer />
-        </HeaderContainer >
     
     </>
  )
