be035ce9d9bc2b39f9967582fe3094ec0ef23925
stevenks17 <stevenks17@gmail.com>
2020-12-01 11:50:51 -0500

    setup firebase import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { GlobalStyles } from './global-styles'
+import { firebase } from './lib/firebase.prod'
 
 ReactDOM.render(
   <>
