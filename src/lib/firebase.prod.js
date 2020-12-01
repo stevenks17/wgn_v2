aab86203d4c9cf1365db089c62b3600bb450ec34
stevenks17 <stevenks17@gmail.com>
2020-12-01 11:45:39 -0500

    firebaseconfig added

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -5,7 +5,16 @@ import 'firebase/auth'
 //seed database
 
 
-const config = {}
+const firebaseConfig = {
+    apiKey: "AIzaSyAsu4l-rK_lwoBwe2Ig_UfGb_D3gParJV4",
+    authDomain: "wgn-v2.firebaseapp.com",
+    databaseURL: "https://wgn-v2.firebaseio.com",
+    projectId: "wgn-v2",
+    storageBucket: "wgn-v2.appspot.com",
+    messagingSenderId: "508320980213",
+    appId: "1:508320980213:web:0dffee4649232c1af1b9bc",
+    measurementId: "G-T2Y10HNW2D"
+}
 
 const firebase = Firebase.initializeApp(config)
 
