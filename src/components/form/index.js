52772954479dd0dff058ebb7adb10886609571a3
stevenks17 <stevenks17@gmail.com>
2020-12-03 13:37:25 -0500

    forgot to add Link/Input/Submit

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -24,3 +24,15 @@ Form.Text = function FormText({children, ...restProps}) {
 Form.TextSmall = function FormTextSmall({children, ...restProps}) {
     return <TextSmall {...restProps}>{children}</TextSmall>
 }
+
+Form.Link = function FormLink({children, ...restProps}) {
+    return <Link {...restProps}>{children}</Link>
+}
+
+Form.Input = function FormInput({children, ...restProps}) {
+    return <Input {...restProps}>{children}</Input>
+}
+
+Form.Submit = function FormSubmit({children, ...restProps}) {
+    return <Submit {...restProps}>{children}</Submit>
+}
