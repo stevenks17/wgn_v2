40e0deaaf6e3646c41d2d4619d497a9c2c80effb
stevenks17 <stevenks17@gmail.com>
2020-12-04 16:58:55 -0500

    added form render and useState

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import  HeaderContainer  from '../containers/header'
 import { FooterContainer } from '../containers/footer'
-
+import { Form } from '../components'
 
 
 export default function Signin() {
-    
+  const [emailAddress, setEmailAddress] = useState()  
+  const [password, setPassword] = useState()  
+  const [error, setError] = useState('')  
+
+  // Check for valid form input
+  // email and password
+
   return (
   <>
-    <HeaderContainer>Hello from the Signin</HeaderContainer>
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Signin</Form.Title>
+      </Form>
+
+    </HeaderContainer>
     <FooterContainer /> 
   </>
   )
