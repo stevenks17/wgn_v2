7d284494c712376667872b2c9d862c786fd378ab
stevenks17 <stevenks17@gmail.com>
2020-12-04 15:38:39 -0500

    setup styled component framework

---
+++ src/components/form/styles/form.js
@@ -0,0 +1,12 @@
+export const Error = styled.div``
+
+export const Base = styled.div``
+
+export const Title = styled.h1``
+
+export const Text = styled.p``
+
+export const TextSmall = styled.p``
+
+
+
