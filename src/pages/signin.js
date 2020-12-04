62ce7a9ff2435d5bc658c5c5817da1c6acd03716
stevenks17 <stevenks17@gmail.com>
2020-12-04 17:31:20 -0500

    added form inputs for email and password

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -9,6 +9,13 @@ export default function Signin() {
   const [password, setPassword] = useState()  
   const [error, setError] = useState('')  
 
+  const isInvalid = password === '' || emailAddress === ''
+  const handleSignin = (event) => {
+    event.preventDefault()
+
+    //firebase stuff here
+    
+  }
   // Check for valid form input
   // email and password
 
@@ -17,6 +24,26 @@ export default function Signin() {
     <HeaderContainer>
       <Form>
         <Form.Title>Signin</Form.Title>
+        {error && <Form.Error></Form.Error>}
+
+        <Form.Base onSubmit={handleSignin} method="POST">
+          <Form.Input
+            placeholder="Email Address"
+            value={emailAddress}
+            onChange={({ target }) => setEmailAddress(target.value)}
+        />
+
+        <Form.Input
+            type="Password"
+            autoComplete="off"
+            placeholder="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+        />
+      <Form.Submit disabled={isInvalid} type="submit">
+        Sign In
+      </Form.Submit>
+        </Form.Base>
       </Form>
 
     </HeaderContainer>
