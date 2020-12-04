6b903e0778da7e272edfa7e1a27f3821bd15916c
stevenks17 <stevenks17@gmail.com>
2020-12-04 16:13:01 -0500

    styled link to underline added input and button

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -8,5 +8,19 @@ export const Text = styled.p``
 
 export const TextSmall = styled.p``
 
+export const Link = styled(ReachRouterLink)`
+    color: white;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline
+    }
+`
+
+export const Input = styled.input``
+
+export const Submit = styled.button``
+
+
 
 
