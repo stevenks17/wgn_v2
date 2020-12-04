a85bf2ebaf4d54107ed89d98f08e828bf66dc476
stevenks17 <stevenks17@gmail.com>
2020-12-04 16:59:20 -0500

    forgot to add imports

--- src/components/form/styles/form.js
+++ src/components/form/styles/form.js
@@ -1,3 +1,9 @@
+import styled from 'styled-components/macro'
+import { Link as ReachRouterLink } from 'react-router-dom'
+
+
+export const Container = styled.div``
+
 export const Error = styled.div``
 
 export const Base = styled.div``
