c63d95eddbd5e5acc8c268d939dfd73315074313
stevenks17 <stevenks17@gmail.com>
2020-12-10 13:05:54 -0500

    added imports

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,6 +1,10 @@
-import React from 'react'
-
-
+import React, {useState, useContext} from 'react'
+import { useHistory } from 'react-router-dom'
+import { FirebaseContext } from '../context/firebase'
+import  HeaderContainer  from '../containers/header'
+import { FooterContainer } from '../containers/footer'
+import { Form } from '../components'
+import * as ROUTES from '../constants/routes'
 
 export default function Signup() {
     
