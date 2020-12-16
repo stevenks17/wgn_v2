1b14f4cf430a1fc65d55764a307e0849f6b60a77
stevenks17 <stevenks17@gmail.com>
2020-12-16 18:48:41 -0500

    User redirect helper setup

---
+++ src/helpers/routes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+
+//react router notes for user redirect
+
+
+export function IsUserRedirect({ user, loggedInPath, children, ...rest }) {
+    return (
+
+    )
+}
