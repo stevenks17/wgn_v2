185f7a3fa9df33f30c26825fc2e548a3b829822b
stevenks17 <stevenks17@gmail.com>
2020-12-16 21:48:28 -0500

    signin redirect passted into signin route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Home, Browse, Signin, Signup} from './pages'
 import * as ROUTES from './constants/routes'
+import { IsUserRedirect } from './helpers/routes'
+
 
 export default function App() {
   return (
     <Router>
       <Route exact path='/signin'>
+        <IsUserRedirect 
+        user={user} 
+        loggedInPath={ROUTES.BROWSE} 
+        path={ROUTES.SIGNIN}
+        >
         <Signin />
+        </IsUserRedirect>
       </Route>
       <Route exact path='/signup'>
         <Signup />
