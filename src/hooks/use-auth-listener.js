7a65759dc8f7e164ef29129463cf4003c3d12e35
stevenks17 <stevenks17@gmail.com>
2020-12-21 18:20:14 -0500

    listener use effect

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -6,4 +6,16 @@ export default function useAuthListener() {
         JSON.parse(localStorage.getItem('authUser'))
     )
     const { firebase } = useContext(FirebaseContext)
+
+    useEffect(() => {
+        const listener = firebase.auth().onAuthStateChanged((authUser) => {
+            if (authUser) {
+                localStorage.setItem('authUser', JSON.stringify(authUser))
+                setUser(authUser)
+            } else {
+                localStorage.removeItem('authUser')
+                setUser(null)
+            }
+        })
+    }, [])
 }
