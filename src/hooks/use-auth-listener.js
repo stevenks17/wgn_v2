ff2ff6e14be85bb04f60c0c6baaed21488871639
stevenks17 <stevenks17@gmail.com>
2020-12-21 18:13:56 -0500

    creted listener and firebase context

--- src/hooks/use-auth-listener.js
+++ src/hooks/use-auth-listener.js
@@ -1,1 +1,9 @@
 import React, { useState, useEffect, useContext } from 'react'
+import { FirebaseContext } from '../context/firebase'
+
+export default function useAuthListener() {
+    const [user, setUser] = useState(
+        JSON.parse(localStorage.getItem('authUser'))
+    )
+    const { firebase } = useContext(FirebaseContext)
+}
