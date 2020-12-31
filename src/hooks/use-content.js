e967bb23cb69869d0622e02c4a75d609fc26d314
stevenks17 <stevenks17@gmail.com>
2020-12-31 12:46:09 -0500

    useEffect to target stored collection in firebase

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -2,5 +2,12 @@ import { useState, useEffect, useContext } from 'react'
 import { FirebaseContext } from '../context/firebase'
 
 export default function useContent(target) {
-    
+    const [content, setContent] = useState([])
+    const { firebase } = useContent(FirebaseContext)
+
+    useEffect(() => {
+        firebase.
+            .firestore()
+            .collection(target)
+    }, [])
 }
