4e68f7306a81a9d9358de1d248830ab76d96d231
stevenks17 <stevenks17@gmail.com>
2020-12-31 12:11:11 -0500

    whiteboarded feature set

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -3,6 +3,10 @@ import React from 'react'
 
 
 export default function Browse() {
+
+  // need series and genres
+  // need to make slideable cards
+  // animate cards to play gameplay videos??? maybe use browse container to house animation
     
   return  (<h1> Hello from the browse</h1>)
 
