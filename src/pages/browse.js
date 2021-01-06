fbf3c63749b2bba63e2bd27a217334a0471aa875
stevenks17 <stevenks17@gmail.com>
2021-01-06 11:23:32 -0500

    testing hook grab from firestore to console log

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { useContent } from '../hooks'
 
 
 export default function Browse() {
 
   // need series and genres
+  const { series } = useContent('series')
+  console.log(series)
   // need to make slideable cards
   // animate cards to play gameplay videos??? maybe use browse container to house animation
     
