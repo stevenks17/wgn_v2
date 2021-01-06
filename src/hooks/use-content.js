1f22ccd4047805c78db3a6c81cca15621be96cf5
stevenks17 <stevenks17@gmail.com>
2021-01-06 10:14:24 -0500

    fixed typo added snapshot get for all content

--- src/hooks/use-content.js
+++ src/hooks/use-content.js
@@ -6,8 +6,15 @@ export default function useContent(target) {
     const { firebase } = useContent(FirebaseContext)
 
     useEffect(() => {
-        firebase.
+        firebase
             .firestore()
             .collection(target)
+            .get()
+            .then((snapshot) => {
+                const allContent = snapshot.docs.map((contentObj) => ({
+                    ...contentObj.data(),
+                    docId: contentObj.id
+                }))
+            })
     }, [])
 }
