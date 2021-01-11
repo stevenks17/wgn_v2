0ed4fa701efb2d2920e4ae8bf318a8ce39a7e2f4
stevenks17 <stevenks17@gmail.com>
2021-01-11 14:25:48 -0500

    added games collection

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -6,7 +6,10 @@ export default function Browse() {
 
   // need series and genres
   const { series } = useContent('series')
+  const { games } = useContent('games')
+
   console.log(series)
+  console.log(games)
   // need to make slideable cards
   // animate cards to play gameplay videos??? maybe use browse container to house animation
     
