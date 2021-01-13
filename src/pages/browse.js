fe9183bded3eb6648204917c66712d2bef62bdff
stevenks17 <stevenks17@gmail.com>
2021-01-13 13:31:20 -0500

    pulled in slide by filtered keys

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useContent } from '../hooks'
-
+import selectionFilter from '../utils/selection-filter'
 
 export default function Browse() {
 
   // need series and genres
   const { series } = useContent('series')
   const { games } = useContent('games')
-
-  console.log(series)
-  console.log(games)
   // need to make slideable cards
+const slides = selectionFilter({ series, games})
+console.log(slides)
   // animate cards to play gameplay videos??? maybe use browse container to house animation
     
   return  (<h1> Hello from the browse</h1>)
