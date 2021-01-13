e332a3e93521dd11f0a10861a1be84561f5a42a5
stevenks17 <stevenks17@gmail.com>
2021-01-13 12:50:35 -0500

    series filter complete??

--- src/utils/selection-map.js
+++ src/utils/selection-map.js
@@ -1,7 +1,26 @@
 export default function selectionMap({series, games}) {
     return {
         series: [
-            {title: 'RPG', data: series.filter((item) => item.genre === 'rpg')}
-        ]
+            {
+                title: 'RPG', 
+                data: series.filter((item) => item.genre === 'RPG'),
+            },
+
+            {
+                title: 'FPS', 
+                data: series.filter((item) => item.genre === 'FPS'),
+            },
+
+            {
+                title: 'Horror', 
+                data: series.filter((item) => item.genre === 'Horror'),
+            },
+
+            {
+                title: 'Strategy', 
+                data: series.filter((item) => item.genre === 'Strategy'),
+            },
+
+        ],
     }
 }
