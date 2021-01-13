27b771915d421e072760b3e4a8c72888fef84914
stevenks17 <stevenks17@gmail.com>
2021-01-13 13:10:32 -0500

    games filter for browse

--- src/utils/selection-map.js
+++ src/utils/selection-map.js
@@ -22,5 +22,28 @@ export default function selectionMap({series, games}) {
             },
 
         ],
+        games: [
+            {
+                title: 'RPG', 
+                data: games.filter((item) => item.genre === 'RPG'),
+            },
+            {
+                title: 'FPS', 
+                data: games.filter((item) => item.genre === 'FPS'),
+            },
+            {
+                title: 'Horror', 
+                data: games.filter((item) => item.genre === 'Horror'),
+            },
+            {
+                title: 'Strategy', 
+                data: games.filter((item) => item.genre === 'Strategy'),
+            },
+            {
+                title: 'Action-Adventure', 
+                data: games.filter((item) => item.genre === 'Action-Adventure'),
+            },
+
+        ]
     }
 }
