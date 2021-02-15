1c11712882dc43ba5852dbc46ad4000f03ec07ee
stevenks17 <stevenks17@gmail.com>
2021-02-15 14:01:55 -0500

    set profile for current user

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react'
+import React, { useState,useContext } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
 
 
 export function BrowseContainer({ slides }) {
-
+    const [profile, setProfile] = useState({})
     const { firebase } = useContext(FirebaseContext)
     const user = firebase.auth().currentUser || {}
 
-    return <SelectProfileContainer user={user} />
+    return <SelectProfileContainer user={user} setProfile={setProfile} 
+    />
 }
