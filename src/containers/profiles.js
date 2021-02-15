92c9f0459864e6c07f7db20f7bb2580e9ba6d729
stevenks17 <stevenks17@gmail.com>
2021-02-15 12:55:11 -0500

    fixed typo

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { Header } from '../components'
+import { Header, Profiles } from '../components'
 import * as ROUTES from '../constants/routes'
 import logo from '../logo2.png'
+
 
 export function SelectProfileContainer({ user, setProfile}) {
     return (
@@ -12,15 +13,15 @@ export function SelectProfileContainer({ user, setProfile}) {
                 </Header.Frame>
             </Header>
 
-            <Profile>
-                <Profile.Title> Logged in as </Profile.Title>
+            <Profiles>
+                <Profiles.Title> Logged in as </Profiles.Title>
                 <Profiles.List>
                     <Profiles.User>
                         <Profiles.Picture src={user.photoURL} />
                         <Profiles.Name>{user.displayName}</Profiles.Name>
                     </Profiles.User>
                 </Profiles.List>
-            </Profile>
+            </Profiles>
         </>
     )
 }
