76591e77e2995f47060d5d5f9fe2b5ae4611212a
stevenks17 <stevenks17@gmail.com>
2021-02-25 13:11:16 -0500

    added loading gif display to profile load

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,12 +1,20 @@
-import React, { useState,useContext } from 'react'
+import React, { useState,useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
 
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({})
+    const [loading, setLoading] = useState(true)
     const { firebase } = useContext(FirebaseContext)
     const user = firebase.auth().currentUser || {}
+
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)
+    }, [profile.displayName])
 
     return <SelectProfileContainer user={user} setProfile={setProfile} 
     />
