e47495517e3763b3565e19c9a0c91433ef2bdc05
stevenks17 <stevenks17@gmail.com>
2021-03-08 14:20:37 -0500

    passed in Header

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useState,useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
-import { Loading } from '../components'
+import { Header, Loading } from '../components'
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({})
@@ -18,9 +18,17 @@ export function BrowseContainer({ slides }) {
     }, [profile.displayName])
 
     return profile.displayName ? (
-        loading  ? (
+        <>
+        {loading  ? (
         <Loading src={user.photoURL} />
-     ) : null
+     ) : (
+        <Loading.ReleaseBody />
+      )}
+
+      <Header src="gamer1">
+          <p>Hello</p>
+        </Header>
+      </>
     ) : (
     <SelectProfileContainer user={user} setProfile={setProfile} 
     /> 
