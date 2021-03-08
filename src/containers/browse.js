2a610b2bf53792d96d4db4f336d9d3bfe0a80476
stevenks17 <stevenks17@gmail.com>
2021-03-08 13:32:31 -0500

    added profile styling

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,7 +1,7 @@
 import React, { useState,useContext, useEffect } from 'react'
 import { SelectProfileContainer } from './profiles'
 import { FirebaseContext } from '../context/firebase'
-
+import { Loading } from '../components'
 
 export function BrowseContainer({ slides }) {
     const [profile, setProfile] = useState({})
@@ -11,11 +11,18 @@ export function BrowseContainer({ slides }) {
 
 
     useEffect(() => {
+        console.log('profile', profile)
         setTimeout(() => {
             setLoading(false)
         }, 3000)
     }, [profile.displayName])
 
-    return <SelectProfileContainer user={user} setProfile={setProfile} 
-    />
+    return profile.displayName ? (
+        loading  ? (
+        <Loading src={user.photoURL} />
+     ) : null
+    ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} 
+    /> 
+    )
 }
