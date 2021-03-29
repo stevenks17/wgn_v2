c12480284904c955bf3ddac39078c66fb61be97b
stevenks17 <stevenks17@gmail.com>
2021-03-29 15:57:47 -0400

    added picture group textlink

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Link as ReactRouterLink} from 'react-router-dom'
-import { Background, ButtonLink, Container, Logo, Feature, Text, FeatureCallOut, } from './styles/header'
+import { Background, ButtonLink, Container, Group, Link, Logo, Feature, Text, FeatureCallOut, Picture, } from './styles/header'
 
 export default function Header({ bg = true, children, ...restProps }) {
     return bg ? (
@@ -24,8 +24,16 @@ Header.Text = function HeaderText({children, ...restProps}) {
     return <Text {...restProps}>{children} </Text>
 }
 
+Header.TextLink = function HeaderTextLink({children, ...restProps}) {
+    return <Link {...restProps}>{children} </Link>
+}
+
 Header.Frame = function HeaderFrame({children, ...restProps}) {
     return <Container {...restProps}>{children}</Container>
+}
+
+Header.Group = function HeaderGroup({children, ...restProps}) {
+    return <Group {...restProps}>{children}</Group>
 }
 
 Header.ButtonLink = function HeaderButtonLink({children, ...restProps}) {
@@ -40,3 +48,6 @@ Header.Logo = function HeaderLogo ({ to, ...restProps}) {
     )
 }
 
+Header.Picture = function HeaderPicture({src, ...restProps}) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />
+}
