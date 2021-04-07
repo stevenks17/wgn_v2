6b30299f126b873f9b42fedf3895a63a214b1941
stevenks17 <stevenks17@gmail.com>
2021-04-07 15:45:00 -0400

    re-ordered components for initialization

--- src/components/header/styles/header.js
+++ src/components/header/styles/header.js
@@ -1,158 +1,111 @@
-import styled from 'styled-components/macro'
-import { Link as ReactRouterLink } from 'react-router-dom'
+import styled from 'styled-components/macro';
+import { Link as ReachRouterLink } from 'react-router-dom';
 
 export const Background = styled.div`
-display: flex;
-flex-direction: column;
-background: url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
-  no-repeat;
-
-  @media(max-width: 1100px) {
-      ${({ dontShowOnSmallViewPort}) => dontShowOnSmallViewPort && 'background: none;'}
+  display: flex;
+  flex-direction: column;
+  background: linear-gradient(to bottom, rgba(0, 0, 0, 0.35), rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.35)), url(${({ src }) => (src ? `../images/misc/${src}.jpg` : '../images/misc/home-bg.jpg')}) top left / cover
+    no-repeat;
+  @media (max-width: 1100px) {
+    ${({ dontShowOnSmallViewPort }) => dontShowOnSmallViewPort && `background: none;`}
   }
-
-`
-
-export const Frame = styled.div`
-
-`
+`;
 
 export const Container = styled.div`
+  display: flex;
+  margin: 0 56px;
+  height: 100px;
+  justify-content: space-between;
+  align-items: center;
+  a {
     display: flex;
-    margin: 0 56px;
-    height: 64px;
-    padding: 18px 0;
-    justify-content: space-between;
-    align-items: center;
-
-    a {
-        display:flex;
-    }
-
-    @media(max-width: 1000px) {
-        margin: 0 30px;
-    }
-`
-
-export const Logo = styled.img`
-    width: 108px;
-    height: 52px;
-    margin-left: -20px;
-
-    @media (min-width: 1449px) {
-        height: 45px;
-        width: 167px;
-    }
-`
-
-export const ButtonLink = styled(ReactRouterLink)`
-    display: block;
-    background-color: #e50914;
-    width: 84px;
-    height: fit-content;
-    color: white;
-    font-size: 15px;
-    border-radius: 3px;
-    padding: 8px 17px;
-    cursor: pointer;
-    text-decoration: none;
-    box-sizing: border-box;
-    margin-left: 13px;
-    margin-top: 6px;
-    &:hover{
-        background-color: #f40612;
-    }
-`
-export const Feature = styled(Container)`
-    padding: 150px 0 500px 0;
-    flex-direction: column;
-    align-items: normal
-    width: 50%;
-
-    @media (max-width: 1100px) {
-        display: none;
-    } 
-
-`
-
-export const Text = styled.p`
-    color: white;
-    font-size: 22px;
-    line-height: normal;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
-    margin: 0;
-`
+  }
+  @media (max-width: 1000px) {
+    margin: 0 30px;
+  }
+`;
 
 export const Link = styled.p`
-    color: white;
-    text-decoration: none;
-    margin-right: 30px;
-    margin-top: 0px;
-    font-weight: ${({active}) => (active === 'true' ? '700': 'normal') };
-    cursor: pointer;
+  color: #fff;
+  text-decoration: none;
+  margin-right: 30px;
+  font-weight: ${({ active }) => (active === 'true' ? '700' : 'normal')};
+  cursor: pointer;
+  &:hover {
+    font-weight: bold;
+  }
+  &:last-of-type {
+    margin-right: 0;
+  }
+`;
 
-    &:hover {
-        font-weight: bold;
-    }
-
-    &: last-of-type {
-        margin-right; 0;
-    }
-`
-
-export const FeatureCallOut = styled.h2`
-    color: white;
-    font-size: 50px;
-    line-height: bold;
-    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
-    margin: 0;
-    margin-bottom: 20px;
-`
 export const Group = styled.div`
-    display: flex;
-    align-items: center;
-`
-
-export const Dropdown = styled.div`
-    display: none;
-    position: absolute;
-    background-color: black;
-    padding: 10px;
-    width: 100px;
-    top: 32px;
-    right: 10px;
-
-    ${Group} :last-of-type ${Link} {
-        cursor: pointer;
-    }
-
-    ${Group} {
-        margin-bottom: 10px;
-
-        &:last-of-type {
-            margin-bottom: 0;
-        }
-        
-    }
-
-    
-
-` 
-
-export const Profile = styled.div`
   display: flex;
   align-items: center;
-  margin-left: 20px;
-  position: relative;
+`;
 
-  button {
+export const SearchInput = styled.input`
+  background-color: rgba(64, 64, 64, 0.5);
+  color: white;
+  border: 1px solid white;
+  transition: width 0.5s;
+  height: 30px;
+  font-size: 14px;
+  border-radius: 4px;
+  margin-left: ${({ active }) => (active === true ? '10px' : '0')};
+  padding: ${({ active }) => (active === true ? '0 10px' : '0')};
+  opacity: ${({ active }) => (active === true ? '1' : '0')};
+  width: ${({ active }) => (active === true ? '200px' : '0px')};
+  &:focus {
+    background-color: rgba(0, 0, 0, 0.8);
+  }
+`;
+
+export const Search = styled.div`
+  display: flex;
+  align-items: center;
+  svg {
+    color: white;
     cursor: pointer;
   }
-  &:hover > ${Dropdown} {
-    display: flex;
-    flex-direction: column;
+  @media (max-width: 700px) {
+    display: none;
   }
-`
+`;
+
+export const SearchIcon = styled.button`
+  cursor: pointer;
+  background-color: transparent;
+  border: 0;
+  outline: 0;
+  height: 32px;
+  width: 32px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  img {
+    filter: brightness(0) invert(1);
+    width: 16px;
+  }
+`;
+
+export const ButtonLink = styled(ReachRouterLink)`
+  display: block;
+  background-color: #e50914;
+  width: 84px;
+  height: fit-content;
+  color: white;
+  border: 0;
+  font-size: 15px;
+  border-radius: 3px;
+  padding: 8px 17px;
+  cursor: pointer;
+  text-decoration: none;
+  &:hover {
+    background: #f40612;
+  }
+`;
 
 export const Picture = styled.button`
   background: url(${({ src }) => src});
@@ -162,3 +115,90 @@ export const Picture = styled.button`
   height: 32px;
   cursor: pointer;
 `;
+
+export const Dropdown = styled.div`
+  display: none;
+  position: absolute;
+  background-color: black;
+  padding: 10px;
+  width: 100px;
+  top: 32px;
+  right: 10px;
+  ${Group}:last-of-type ${Link} {
+    cursor: pointer;
+  }
+  ${Group} {
+    margin-bottom: 10px;
+    &:last-of-type {
+      margin-bottom: 0;
+    }
+    ${Link} {
+      cursor: pointer;
+    }
+    ${Picture} {
+      cursor: default;
+    }
+  }
+  button {
+    margin-right: 10px;
+  }
+  p {
+    font-size: 12px;
+    margin-bottom: 0;
+    margin-top: 0;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  align-items: center;
+  margin-left: 20px;
+  position: relative;
+  button {
+    cursor: pointer;
+  }
+  &:hover > ${Dropdown} {
+    display: flex;
+    flex-direction: column;
+  }
+`;
+
+export const Feature = styled(Container)`
+  padding: 150px 0 500px 0;
+  flex-direction: column;
+  align-items: normal;
+  width: 50%;
+  @media (max-width: 1100px) {
+    display: none;
+  }
+`;
+
+export const FeatureCallOut = styled.h2`
+  color: white;
+  font-size: 50px;
+  line-height: normal;
+  font-weight: bold;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  margin: 0;
+  margin-bottom 20px;
+`;
+
+export const Text = styled.p`
+  color: white;
+  font-size: 22px;
+  line-height: normal;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.45);
+  
+
+`;
+
+export const Logo = styled.img`
+  height: 52px;
+  width: 108px;
+  margin-right: 40px;
+  @media (min-width: 1449px) {
+    height: 45px;
+    width: 167px;
+  }
+`;
+
